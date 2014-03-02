7ec31d18c7bd0b178bdf94ff332bdf8d16701ce1
Craige McWhirter <craige@mcwhirter.com.au>
2014-03-02 21:43:27 +1100

    Updated the Roadmap

--- skippy.py
+++ skippy.py
@@ -15,19 +15,34 @@ import os.path
 # Import functions to manipulate time values
 import time
 # Import the ability to open URLs
-import urllib
+import urllib2
 # Import the ability to match filenames with shell patterns
 import fnmatch
-# ???
+# Import the basis for parsing text files formatted in HTML
 import HTMLParser
 # Import the capability to do logging
 import logging
 # Import additional logging handlers
 import logging.handlers
 
-def getURL():
-	"""Gets a URL from RAW INPUT and prints it"""
-	url = raw_input("Feed me a URL: ")
-	print "%s" % url
+#def getURL():
+#	"""Gets a URL from RAW INPUT and prints it"""
+#	url = raw_input("Feed me a URL: ")
+#	print "%s" % url
+#	"""Take the same URL and get the headers"""
+#	URLheaders = urllib.urlopen(url)
+#	print "%s" % URLheaders
 
-getURL()
+# getURL()
+
+def getPage():
+	url = raw_input("Feed me a URL: ")
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	return response.read()
+ 
+if __name__ == "__main__":
+	namesPage = getPage()
+	print namesPage
+
+getPage()
