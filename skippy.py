c845114a730ef343aaa26b37b99053aad8518904
Craige McWhirter <craige@mcwhirter.com.au>
2014-03-04 00:34:56 +1100

    Now scrapes URLs from channel

--- skippy.py
+++ skippy.py
@@ -27,6 +27,8 @@ import logging.handlers
 # Import a nifty HTML parser
 from BeautifulSoup import BeautifulSoup
 #from util.BeautifulSoup import UnicodeDammit
+# Import regular expression capabilities
+import re
 
 # Have the bot join IRC
 # Import some necessary libraries.
@@ -34,7 +36,7 @@ import socket
 
 # Some basic variables used to configure the bot        
 server = "irc.freenode.net" # Server
-channel = "#taslug" # Channel
+channel = "#STBK" # Channel
 botnick = "STBK" # Your bots nick
 
 
@@ -49,6 +51,12 @@ def joinchan(chan): # This function is used to join channels.
 
 def hello(): # This function responds to a user that inputs "Hello Mybot"
   ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
+
+def getPage(url):
+	"""Gets a URL from RAW INPUT and reads it"""
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	return response.read()
                   
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
@@ -57,28 +65,31 @@ ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the b
 
 joinchan(channel) # Join the channel using the functions we previously defined
 
+ircmsg = ircsock.recv(2048) # receive data from the server
+ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+
+while ircmsg.find(channel) == -1:
+	ircmsg = ircsock.recv(2048) # receive data from the server
+	ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+
 while 1: # Be careful with these! it might send you to an infinite loop
-  ircmsg = ircsock.recv(2048) # receive data from the server
-  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
-  print(ircmsg) # Here we print what's coming from the server
+	ircmsg = ircsock.recv(2048) # receive data from the server
+	ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+	print(ircmsg) # Here we print what's coming from the server
 
-  if ircmsg.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
-    hello()
+	# Extract the title from the page and print it
+	# if ircmsg.find ("http") != -1:
+	url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ircmsg)
+	if len(url) > 0:
+		titlePage = BeautifulSoup(getPage(url[0]))
+		for  child in titlePage.title:
+			sendmsg(channel, child)
 
-  if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
-    ping()
+	if ircmsg.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
+		hello()
+
+	if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
+		ping()
 # Read the channel for URLS
 # Feed the URLs into the below functions
 
-def getPage():
-	"""Gets a URL from RAW INPUT and reads it"""
-	url = raw_input("Feed me a URL: ")
-	req = urllib2.Request(url)
-	response = urllib2.urlopen(req)
-	return response.read()
- 
-# Extract the title from the page and print it
-if __name__ == "__main__":
-	titlePage = BeautifulSoup(getPage())
-	for  child in titlePage.title:
-		print(child)
